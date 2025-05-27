35d8fcfd0b6d512000f5ae2c29bcc3e41d0141da
DerrekNguyen <mnguye21@kent.edu>
2025-05-27 16:05:36 -0400

    bears on screen

--- src/drawFunctions/animals.js
+++ src/drawFunctions/animals.js
@@ -2,9 +2,24 @@ import store from "../stateManager/store";
 import { Bear, Deer, Rabbit } from "../models/Species";
 import { addBear, addDeer, removeBear, removeDeer } from "../stateManager/slices/animalSlices";
 
-function drawAnimal() {
-    store.dispatch(addBear(new Bear().toObject()));
-    console.log(store.getState());
+function drawAnimal(p, img) {
+    if (!img) return;
+
+    const animals = store.getState().animals;
+
+    if (animals.bears.length === 0) {
+        store.dispatch(addBear(new Bear().toObject()));
+        store.dispatch(addBear(new Bear().toObject()));
+        store.dispatch(addBear(new Bear().toObject()));
+    }
+
+    let x = 0;
+    let y = 100;
+
+    animals.bears.forEach(element => {
+        p.image(img, x, y);
+        x += 150;
+    });
 }
 
 export default drawAnimal;
